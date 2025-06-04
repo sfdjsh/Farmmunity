03c5f4b1d76fbe2da55652e8d37a10f69bd25351
sfdjsh <sfdjsh@naver.com>
2025-06-04 19:29:37 +0900

    server 폴더 재설치

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const cors = require("cors");
+const cors = require('cors')
 
 const app = express();
-app.use(cors());
+app.use(cors())
 
 app.get("/test", (req, res) => {
   res.json({
-    test: "hello!",
+    test: 'hello!'
   });
 });
 
