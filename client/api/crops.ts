13408bd3468fd35d0db391c4a23a42f1e15b6f48
sfdjsh <sfdjsh@naver.com>
2025-06-11 19:16:39 +0900

    BASE_URL env 환경 설정

--- client/api/crops.ts
+++ client/api/crops.ts
@@ -1,12 +1,15 @@
+import {BASE_URL} from "@env"
+
 // 농작물 이름 조회
 export const getCropsNameApi = async () => {
-    const data = await fetch("http://10.0.2.2:3000/crops/name")
+    const data = await fetch(`${BASE_URL}/crops/name`)
     const res = await data.json();
     return res.data
 }
 
+// 선택된 농작물 정보 조회
 export const getCropsInfoApi = async (name: string) => {
-    const data = await fetch(`http://10.0.2.2:3000/select/crop?name=${name}`)
+    const data = await fetch(`${BASE_URL}/select/crop?name=${name}`)
     const res = await data.json();
     return res.data
 }
