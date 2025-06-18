e16f0cf9a3732564d7d68687d8be5e65e0bf6a7d
sfdjsh <sfdjsh@naver.com>
2025-06-18 20:41:45 +0900

    FE_02 : 게시글 상세 조회 API 연동

--- client/api/articleApi.ts
+++ client/api/articleApi.ts
@@ -5,3 +5,9 @@ export const getTotalArticlesApi = async (orderBy: string) => {
     const res = await data.json();
     return res.data
 }
+
+export const getDetailArticleApi = async (id: number) => {
+    const data = await fetch(`${BASE_URL}/article/${id}`)
+    const res = await data.json();
+    return res.data
+}
--- client/pages/ArticleDetailPage.tsx
+++ client/pages/ArticleDetailPage.tsx
@@ -1,6 +1,8 @@
 import { View, Text } from "react-native";
 import { RouteProp } from "@react-navigation/native";
 import { RootStackParamList } from "../component/article/TotalArticle";
+import { useEffect, useState } from "react";
+import { getDetailArticleApi } from "../api/articleApi";
 
 type ArticleDetailRouteProp = RouteProp<RootStackParamList, "ArticleDetail">;
 type Props = {
@@ -8,12 +10,30 @@ type Props = {
 };
 
 const ArticleDetailPage = ({ route }: Props) => {
+  const [articleData, setArticleData] = useState({});
+  const [commentsData, setCommentsData] = useState([]);
   const id = route?.params?.id;
-  console.log(id);
+
+  useEffect(() => {
+    const fetchDetailArticle = async () => {
+      const DetailArticleData = await getDetailArticleApi(id);
+      setArticleData(DetailArticleData.article);
+      setCommentsData(DetailArticleData.comment);
+    };
+    fetchDetailArticle();
+  }, [id]);
+
+  console.log("articleData:", articleData);
+  console.log(commentsData);
 
   return (
     <View>
-      <Text>디테일 페이지 : {id}</Text>
+      {articleData && (
+        <>
+          <Text>{articleData.category}</Text>
+          <Text>디테일 페이지 : {id}</Text>
+        </>
+      )}
     </View>
   );
 };
